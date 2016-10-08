4906a7e1836446a1f91b996a9731d58f7005c525
Serg Chernata <serg@mycarjoy.com>
2016-10-08 10:19:18 -0400

    solved but too verbose and time consuming

--- 02b.py
+++ 02b.py
@@ -2,8 +2,9 @@ def answer(total_lambs):
     stingy = []
     generous = []
     max = int(total_lambs / 2)
-    x = 1
+    last = 1
 
+    x = 1
     while x < max:
         rule_1 = x == 1 and 1 not in stingy
         rule_2 = len(stingy) < 1 or stingy[-1] * 2 >= x
@@ -14,10 +15,22 @@ def answer(total_lambs):
         else:
             x += 1
 
-        print(stingy)
-        print('x: ',x,' 1: ',rule_1,' 2: ',rule_2,' 3: ',rule_3,)
+    x = 1
+    while x < max:
+        rule_1 = x == 1 and 1 not in generous
+        rule_2 = len(generous) < 1 or generous[-1] * 2 >= x
+        rule_3 = len(generous) < 2 or len(generous) >= 2 and x >= generous[-1] + generous[-2]
 
-    #print(generous)
+        if rule_1 or rule_2 and rule_3:
+            if len(generous) == 0 or x == max - 1:
+                generous.append(x)
+            last = x
+        else:
+            if last not in generous:
+                generous.append(last)
+
+        x += 1
+
     print(len(stingy) - len(generous))
 
-answer(10)
+answer(143)
