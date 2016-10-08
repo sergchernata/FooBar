464cd08c56cdd1ccc56d03eabd5510704fd0c0a9
Serg Chernata <serg@mycarjoy.com>
2016-10-08 19:25:56 -0400

    lol out

--- Answer.java
+++ Answer.java
@@ -1,4 +1,4 @@
-package com.google.challenges;
+//package com.google.challenges;
 
 public class Answer {
     public static int answer(int start, int length) {
@@ -6,14 +6,11 @@ public class Answer {
         int count = start;
         int checksum = -1;
 
-        for(int y=length; y>1; y--)
-        {
+        for(int y=length; y>1; y--){
             int line = y + 1;
 
-            for(int x = 0; x<length+1; x++)
-            {
-                if(line > 0)
-                {
+            for(int x = 0; x<length+1; x++){
+                if(line > 0){
                     if(checksum > -1){
                         checksum ^= count;
                     }else{
