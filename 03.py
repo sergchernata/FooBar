f9188a579cf9ab8ed8578009886e9379af8a8e45
Serg Chernata <serg@mycarjoy.com>
2016-10-08 14:49:53 -0400

    solved, but too slow

--- 03.py
+++ 03.py
@@ -1,4 +1,22 @@
-def answer():
+def answer(start, length):
+    count = start
+    checksum = -1
+    x_axis = range(length)
+    y_axis = reversed(range(length + 1))
 
+    for y in y_axis:
+        line = y
 
-print(answer())
+        for x in x_axis:
+            if line > 0:
+                if checksum > -1:
+                    checksum ^= count
+                else:
+                    checksum = start
+                line -= 1
+
+            count += 1
+
+    return checksum
+
+print(answer(17,4))
