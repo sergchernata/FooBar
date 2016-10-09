36d0732e6941c552215f22dd07bff3245f3fdead
Serg Chernata <serg@mycarjoy.com>
2016-10-09 12:59:27 -0400

    passing 3 tests out of ten with one loop

--- 03.py
+++ 03.py
@@ -1,10 +1,8 @@
-import time
-
 def answer(start, length):
     count = start
     checksum = -1
     x_axis = range(length + 1)
-    y_axis = iter(reversed(range(length)))
+    y_axis = reversed(range(length))
 
     for y in y_axis:
         line = y + 1
@@ -22,7 +20,27 @@ def answer(start, length):
 
     return checksum
 
-start = time.time()
+def answer(start, length):
+    checksum = skip = -1
+    end = start + 4 * (length - 1) + 1
+    line = prev = length
+
+    for x in range(start,end):
+
+        if line == 0:
+            if skip == -1: skip = length - prev
+
+            if skip <= 0:
+                checksum ^= x
+                line = prev = prev - 1
+                skip = -1
+                line -= 1
+            else:
+                skip -= 1
+        else:
+            checksum = checksum^x if checksum != -1 else x
+            line -= 1
+
+    return checksum
+
 print(answer(17,4))
-end = time.time()
-print(end - start)
