b31c4e4288962bf3ea1797db294578d7c933226d
Serg Chernata <sergchernata@gmail.com>
2016-10-10 10:34:08 -0400

    combined 3rd and 2nd, still too slow

--- 03.py
+++ 03.py
@@ -30,27 +30,25 @@ def answer(start, length):
     chksm = 0
     end = start + length * (length - 1)
     line = prev_line = length
-
+    
     while start <= end:
         if line == 0:
             start += length - prev_line
-            chksm ^= start
             line = prev_line = prev_line - 1
         else:
-            chksm ^= start
-
-        start += 1
-        line -= 1
+            chksm ^= functools.reduce(xor, list(range(start,start+line)))
+            start += line
+            line = 0
 
     return chksm
 
 # let's get weird with it
-def answer(start, length):
-    c = IT.count(0)
-    s = start
-    l = length
-    nums = [list(range(s+l*n, s+l*n+l-next(c))) for n in range(l)]
-    nums = [item for sublist in nums for item in sublist]
-    return functools.reduce(xor, nums)
+# def answer(start, length):
+#     c = IT.count(0)
+#     s = start
+#     l = length
+#     nums = [list(range(s+l*n, s+l*n+l-next(c))) for n in range(l)]
+#     nums = [item for sublist in nums for item in sublist]
+#     return functools.reduce(xor, nums)
 
 print(answer(17,4))
