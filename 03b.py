5621dff0f3ec84fa6e2e20ef931ad6805e429b86
Serg Chernata <serg@mycarjoy.com>
2016-10-10 20:44:03 -0400

    working solution, but too slow

--- 03b.py
+++ 03b.py
@@ -1,18 +1,20 @@
 def answer(M, F):
     cycles = 0
-    m = f = 1
-    m_goal = int(M)
-    f_goal = int(F)
+    m = int(M)
+    f = int(F)
 
-    while m <= m_goal and f <= f_goal:
-        m = f + m
+    while (m > 1 or f > 1) and (m > 0 and f > 0):
+        if m > f:
+            m = m - f
+        elif m == f:
+            break
+        else:
+            f = f - m
         cycles += 1
 
-
-    if m == m_goal and f == f_goal:
+    if m == f == 1:
         return str(cycles)
     else:
         return 'impossible'
 
-print(10**50)
-print(answer('2','1'))
+print(answer('4','7'))
