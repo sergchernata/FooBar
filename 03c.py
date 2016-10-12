a8b535b3543d4729dd109873c3755d9386fb851c
Serg Chernata <serg@mycarjoy.com>
2016-10-12 18:41:04 -0400

    stop looking at entire list, slice

--- 03c.py
+++ 03c.py
@@ -4,23 +4,33 @@ import time
 def answer(l):
 	triples = 0
 	l.reverse()
-	processed = []
+	index = 0
 
 	for num in l:
-		subset = [n for n in l if num%n is 0]
+		subset = [n for n in l[index:] if num%n is 0]
 		subset.pop(0)
 
 		if num not in processed:
 			combo = combinations(subset, 2)
 			triples += sum(1 for d in combo if d[0]%d[1] is 0)
-			processed.append(num)
-	
+
+		index += 1
+
 	return triples
 
 #num_list = list(range(1,9999))
-num_list = [1, 2, 2, 4]
+num_list = [1,2,2,2,2,2]
 
 start = time.time()
 print(answer(num_list))
 end = time.time()
 print((end - start))
+
+# 2d,2c,2b
+# 2d,2b,2a
+# 2d,2a,1
+# 2d,2b,1
+# 2d,2c,1
+# 2c,2b,2a
+# 2c,2a,1
+# 2b,2a,1
