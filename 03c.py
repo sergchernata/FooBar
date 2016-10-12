1a99e24a13a2378d8c8f10c95548cbaf64c89a76
Serg Chernata <serg@mycarjoy.com>
2016-10-11 20:12:11 -0400

    oops

--- 03c.py
+++ 03c.py
@@ -1,5 +1,17 @@
 def answer(l):
     triples = 0
+    nums = list(set(l))
+
+    if len(nums) == 1:
+        return 1
+    else:
+        nums.reverse()
+        for x in nums:
+            high = x / 2
+            even = x % 2 == 0
+
+            if even:
+                triples += 1
 
     return triples
 
