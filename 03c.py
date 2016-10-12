6107286cd9f931ab568c5e5de9c02770e74debbf
Serg Chernata <sergchernata@gmail.com>
2016-10-12 11:36:10 -0400

    cleanup

--- 03c.py
+++ 03c.py
@@ -14,35 +14,13 @@ def answer(l):
 		if x not in processed:
 			divisors = combinations(subset, 2)
 			triples += sum(1 for d in divisors if d[0]%d[1]==0)
-			#processed.append(x)
-			# while len(subset) > 1:
-			# 	divisor = str(subset[0])+','+str(subset[cursor])
-			# 	divides = subset[0] % subset[cursor] == 0
-			# 	if divides and divisor not in divisors:
-			# 		divisors.append(divisor)
-
-			# 	if cursor < len(subset) - 1:
-			# 		cursor += 1
-			# 	else:
-			# 		cursor = 1
-			# 		subset.pop(0)
-
-			# triples += len(divisors)
-			# processed.append(x)
 	
 	return triples
 
 num_list = list(range(1,9999))
 #num_list = [1, 2, 3, 4, 5, 6, 12]
-#print(num_list)
+
 start = time.time()
 print(answer(num_list))
 end = time.time()
 print((end - start))
-
-# 6,6,3
-# 6,6,2
-# 6,6,1
-# 6,3,1
-# 6,2,1
-# 4,2,1
