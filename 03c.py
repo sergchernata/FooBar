f43904a07716888fd547338ada61c857e628bd18
Serg Chernata <serg@mycarjoy.com>
2016-10-11 22:44:29 -0400

    smarter code, still passing only 2/5 though

--- 03c.py
+++ 03c.py
@@ -4,22 +4,42 @@ def answer(l):
 
     if len(nums) == 1:
         return 1
+    elif len(nums) == 2:
+        return 0
     else:
         nums.reverse()
         for x in nums:
+            cursor = 1
             divisors = []
-
             if not x % 2:
-                divisors.append(x // 2)
-                if divisors[-1] in nums:
-                    subset = nums[nums.index(divisors[-1]):]
-                    while len(subset) > 0:
-                        if x % subset[0] == 0 and subset[0] not in divisors:
-                            divisors.append(subset[0])
+                subset = [n for n in nums if x%n==0 and not x == n]
+
+                while len(subset) > 1:
+                    divisor = str(x)+','+str(subset[0])+','+str(subset[cursor])
+                    divides = subset[0] % subset[cursor] == 0
+                    if divides and divisor not in divisors:
+                        divisors.append(divisor)
+
+                    if cursor < len(subset) - 1:
+                        cursor += 1
+                    else:
+                        cursor = 1
                         subset.pop(0)
 
-                    triples += len(divisors) - 1
+                triples += len(divisors)
+            print(divisors)
+        return triples
 
-    return triples
+print(answer([2, 3, 4, 5, 6, 12]))
 
-answer([1, 2, 3, 4, 5, 6])
+
+# 12,6,3
+# 12,6,2
+# 12,6,1
+# 12,4,2
+# 12,4,1
+# 12,3,1
+# 12,2,1
+# 6,3,1
+# 6,2,1
+# 4,2,1
