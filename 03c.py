69105b4455b667edc0eee3ad0c65ec77cdc5551a
Serg Chernata <sergchernata@gmail.com>
2016-10-12 11:35:05 -0400

    much shorter and 2 seconds quicker

--- 03c.py
+++ 03c.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 import time
 
 def answer(l):
@@ -7,30 +8,32 @@ def answer(l):
 	processed = []
 
 	for x in nums:
-		cursor = 1
-		divisors = []
 		subset = [n for n in nums if x%n==0]
 		subset.pop(0)
 
 		if x not in processed:
-			while len(subset) > 1:
-				divisor = str(subset[0])+','+str(subset[cursor])
-				divides = subset[0] % subset[cursor] == 0
-				if divides and divisor not in divisors:
-					divisors.append(divisor)
+			divisors = combinations(subset, 2)
+			triples += sum(1 for d in divisors if d[0]%d[1]==0)
+			#processed.append(x)
+			# while len(subset) > 1:
+			# 	divisor = str(subset[0])+','+str(subset[cursor])
+			# 	divides = subset[0] % subset[cursor] == 0
+			# 	if divides and divisor not in divisors:
+			# 		divisors.append(divisor)
 
-				if cursor < len(subset) - 1:
-					cursor += 1
-				else:
-					cursor = 1
-					subset.pop(0)
+			# 	if cursor < len(subset) - 1:
+			# 		cursor += 1
+			# 	else:
+			# 		cursor = 1
+			# 		subset.pop(0)
 
-			triples += len(divisors)
-			processed.append(x)
-
+			# triples += len(divisors)
+			# processed.append(x)
+	
 	return triples
 
-num_list = list(range(1,9999))#[1, 2, 3, 4, 5, 6, 12]
+num_list = list(range(1,9999))
+#num_list = [1, 2, 3, 4, 5, 6, 12]
 #print(num_list)
 start = time.time()
 print(answer(num_list))
