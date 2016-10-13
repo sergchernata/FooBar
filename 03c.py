3d3c78ae4aaebb1e8c4a47d5e8412a38bd19df84
Serg Chernata <sergchernata@gmail.com>
2016-10-13 16:06:52 -0400

    fastest execution, still too slow

--- 03c.py
+++ 03c.py
@@ -1,4 +1,6 @@
 from itertools import combinations
+from functools import reduce
+from math import sqrt
 import time
 
 # sexy but slow as fuck
@@ -7,20 +9,29 @@ def answer(l):
 	combo = combinations(l, 3)
 	return sum(1 for d in combo if d[0]%d[1] is 0 and d[1]%d[2] is 0)
 
+#@profile
+def factors(n):
+	step = 2 if n%2 else 1
+	return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
+
 # fastest
+#@profile
 def answer(l):
 	triples = 0
 	l.reverse()
 	index = 0
 
 	for num in l:
-		subset = [n for n in l[index:] if num%n is 0]
-
+		all_factors = factors(num)
+		subset = l[index:]
+		subset = [x for x in subset if x in all_factors]
+		
 		if len(subset) > 1:
 			subset.pop(0)
 			combo = combinations(subset, 2)
 			triples += sum(1 for d in combo if d[0]%d[1] is 0)
-			index += 1
+		
+		index += 1
 
 	return triples
 
