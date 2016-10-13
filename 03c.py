6347eee5e1975d9e0a9d8f85ea809a785617291b
Serg Chernata <serg@mycarjoy.com>
2016-10-12 22:45:28 -0400

    small tweak

--- 03c.py
+++ 03c.py
@@ -8,24 +8,24 @@ def answer(l):
 	return sum(1 for d in combo if d[0]%d[1] is 0 and d[1]%d[2] is 0)
 
 # fastest
-# def answer(l):
-# 	triples = 0
-# 	l.reverse()
-# 	index = 0
+def answer(l):
+	triples = 0
+	l.reverse()
+	index = 0
 
-# 	for num in l:
-# 		subset = [n for n in l[index:] if num%n is 0]
-# 		subset.pop(0)
+	for num in l:
+		subset = [n for n in l[index:] if num%n is 0]
 
-# 		if len(subset) > 1:
-# 			combo = combinations(subset, 2)
-# 			triples += sum(1 for d in combo if d[0]%d[1] is 0)
-# 			index += 1
+		if len(subset) > 1:
+			subset.pop(0)
+			combo = combinations(subset, 2)
+			triples += sum(1 for d in combo if d[0]%d[1] is 0)
+			index += 1
 
-# 	return triples
+	return triples
 
-#num_list = list(range(1,9999))
-num_list = [1,2,3,4,5,6,6]
+num_list = list(range(1,9999))
+#num_list = [1,2,3,4,5,6,12]
 
 start = time.time()
 print(answer(num_list))
