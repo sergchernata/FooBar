a7a6a36c4dd070edf1945bad87b74ed861a3f313
Serg Chernata <serg@mycarjoy.com>
2016-10-12 22:03:52 -0400

    added trollop short version

--- 03c.py
+++ 03c.py
@@ -1,6 +1,17 @@
 from itertools import combinations
 import time
 
+# sexy but basically slow as fuck
+def answer(l):
+	triples = 0
+	l.reverse()
+
+	combo = combinations(l, 3)
+	triples += sum(1 for d in combo if d[0]%d[1] is 0 and d[1]%d[2] is 0)
+
+	return triples
+
+# fastest
 def answer(l):
 	triples = 0
 	l.reverse()
@@ -9,26 +20,18 @@ def answer(l):
 	for num in l:
 		subset = [n for n in l[index:] if num%n is 0]
 		subset.pop(0)
-		combo = combinations(subset, 2)
-		triples += sum(1 for d in combo if d[0]%d[1] is 0)
 
-		index += 1
+		if len(subset) > 1:
+			combo = combinations(subset, 2)
+			triples += sum(1 for d in combo if d[0]%d[1] is 0)
+			index += 1
 
 	return triples
 
 #num_list = list(range(1,9999))
-num_list = [1,2,2,2,2,2]
+num_list = [1,2,3,4,5,6,6]
 
 start = time.time()
 print(answer(num_list))
 end = time.time()
 print((end - start))
-
-# 2d,2c,2b
-# 2d,2b,2a
-# 2d,2a,1
-# 2d,2b,1
-# 2d,2c,1
-# 2c,2b,2a
-# 2c,2a,1
-# 2b,2a,1
