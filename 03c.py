c99288327e8da6f9bfdec1134e53b5e09c4d3522
Serg Chernata <serg@mycarjoy.com>
2016-10-13 21:59:41 -0400

    hmm

--- 03c.py
+++ 03c.py
@@ -14,7 +14,6 @@ def factors(n):
 	step = 2 if n%2 else 1
 	return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
 
-# fastest
 #@profile
 def answer(l):
 	triples = 0
@@ -25,12 +24,12 @@ def answer(l):
 		all_factors = factors(num)
 		subset = l[index:]
 		subset = [x for x in subset if x in all_factors]
-		
+
 		if len(subset) > 1:
 			subset.pop(0)
 			combo = combinations(subset, 2)
 			triples += sum(1 for d in combo if d[0]%d[1] is 0)
-		
+
 		index += 1
 
 	return triples
