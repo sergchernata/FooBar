d32b0d553e5c1eb886b0972cd97b93def9b214c2
Serg Chernata <serg@mycarjoy.com>
2016-10-15 13:37:01 -0400

    organized

--- 03c.py
+++ 03c.py
@@ -46,7 +46,7 @@ def answer(numbers):
 	# if we have only one unique number
 	# or if all numbers are actual divisors already
 	# then add their counts and use the formula
-	if len(counts) is 1 or are_divisors(counts):
+	if len(counts) == 1 or are_divisors(counts):
 		v = sum(counts.values())
 		return yeah_science(v)
 
@@ -57,23 +57,27 @@ def answer(numbers):
 
 		uniques = set(subset)
 		v = len(subset)
-		if v > 2 and (len(uniques) is 1 or are_divisors(uniques)):
+		if v > 2 and (len(uniques) == 1 or are_divisors(uniques)):
 			triples += yeah_science(v)
-			numbers = [item for item in numbers if item != n]
 			counts[n] = 0
 
 		else:
 			triples += combinations(subset)
 			counts[n] -= 1
 
-		if counts[n] is 0:
+		if counts[n] == 0:
 			del counts[n]
 			v = sum(counts.values())
 			if v > 2 and are_divisors(counts):
 				return triples + yeah_science(v)
 
+		numbers = [item for item in numbers if item != n]
+
 	return triples
 
+#-*-*-*-*-*-*-*-*-*-*-*-*-#
+# benchmarking
+#-*-*-*-*-*-*-*-*-*-*-*-*-#
 #num_list = list(range(1,9999))
 # num_list = []
 # for _ in range(1000):
@@ -81,24 +85,41 @@ def answer(numbers):
 # num_list.append(3)
 # num_list.append(7)
 #num_list = [1,1,1,1,1,1,1,3,3,7,7]
-
-print(answer([1,1,1]) is 1)
-print(answer([1,2,3,4,5,6]) is 3)
-print(answer([1,2,3,4,5,6,6]) is 8)
-print(answer([1,2,3,4,5,6,12]) is 10)
-
-# testing the math
-s = answer([1,1,1,1,1,1,1,3,3,7,7])
-a = answer([1,1,1,1,1,1,1,3,3])
-b = answer([1,1,1,1,1,1,1,7,7])
-print(s, a, b, ' - ', s is a + b)
-
-s = answer([1,1,1,1,1,1,1,3,7])
-a = answer([1,1,1,1,1,1,1,3])
-b = answer([1,1,1,1,1,1,1,7])
-print(s, a, b, ' - ', s is a + b)
-
 start = time.time()
 #print(answer(num_list))
 end = time.time()
 print((end - start))
+
+#-*-*-*-*-*-*-*-*-*-*-*-*-#
+# test cases
+#-*-*-*-*-*-*-*-*-*-*-*-*-#
+print(answer([1,1,1]) == 1)
+print(answer([1,1,1,1]) == 4)
+print(answer([1,2,3,4,5,6]) == 3)
+print(answer([1,2,3,4,5,6,6]) == 8)
+print(answer([1,2,3,4,5,6,12]) == 10)
+
+s = answer([1,1,1,1,1,1,1,3,3,7,7])
+a = answer([1,1,1,1,1,1,1,3,3])
+b = answer([1,1,1,1,1,1,1,7,7])
+print(s, a, b, ' - ', s == a + b)
+
+s = answer([1,1,1,1,1,1,1,3,3,3,3,3,3,12,30,90])
+a = answer([1,1,1,1,1,1,1,3,3,3,3,3,3,12])
+b = answer([1,1,1,1,1,1,1,3,3,3,3,3,3,30,90])
+print(s, a, b, ' - ', s == a + b)
+
+s = answer([1,1,2,2,3,3,4,4,5,5,6,6])
+a = answer([1,1,2,2,3,3,6,6])
+b = answer([1,1,2,2,4,4])
+print(s, a, b, ' - ', s == a + b)
+
+s = answer([1,2,3,4,5,6])
+a = answer([1,2,3,6])
+b = answer([1,2,4])
+print(s, a, b, ' - ', s == a + b)
+
+s = answer([1,1,1,1,1,1,1,3,7])
+a = answer([1,1,1,1,1,1,1,3])
+b = answer([1,1,1,1,1,1,1,7])
+print(s, a, b, ' - ', s == a + b)
