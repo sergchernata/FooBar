53da83d8111867395742155bf06589d6d4426721
Serg Chernata <serg@mycarjoy.com>
2016-10-22 12:04:22 -0400

    set up example problem

--- 04.py
+++ 04.py
@@ -1,2 +1,13 @@
 def answer(entrances, exits, path):
-    # your code here
+    mask = [list(p) for p in path]
+
+    for room in path:
+
+
+    return sum([sum(path[e]) for e in exits])
+
+
+entrances = [0, 1]
+exits = [4, 5]
+path = [[0, 0, 4, 6, 0, 0], [0, 0, 5, 2, 0, 0], [0, 0, 0, 0, 4, 4], [0, 0, 0, 0, 6, 6], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+print(answer(entrances, exits, path))
